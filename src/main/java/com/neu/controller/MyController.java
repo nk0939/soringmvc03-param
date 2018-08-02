3faee2abcfcf972bdc0a9bdfda4e9b728306fb80
zy8985780 <39935536+zy8985780@users.noreply.github.com>
2018-08-02 22:17:33 +0800

    aaaa

---
+++ src/main/java/com/neu/controller/MyController.java
@@ -0,0 +1,18 @@
+package com.neu.controller;
+
+import com.neu.bean.Student;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MyController {
+    @RequestMapping("/hello.do")
+    public ModelAndView doSome(Student stu) {
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("stu",stu);
+        mv.setViewName("/show.jsp");
+        return mv;
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" import="java.util.*" pageEncoding="utf-8" %>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+<center>
+    <form action="hello.do" method="post">
+        姓名:<input type="text" name="name"/><br><br>
+        年龄:<input type="text" name="age"/><br><br>
+        学校名称:<input type="text" name="school.name"/><br><br>
+        学校地址:<input type="text" name="school.address"/><br><br>
+        <input type="submit" value="注册">
+    </form>
+</center>
+
+</body>
+</html>
